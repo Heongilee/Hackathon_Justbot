2472146c3534aee40fb7ea1257c4fe0a59410f14
weedsib123 <jaeung95@naver.com>
2020-07-02 13:48:12 +0900

    07021348
    Signed-off-by: weedsib123 <jaeung95@naver.com>

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dialogflow = require('@google-cloud/dialogflow');
 const sessionClient = new dialogflow.SessionsClient()
 const uuid = require('uuid');
 const chatbot=require('./chatbot.js')
-const bot=new chatbot('newagent-1-impflf','./newagent-1-impflf-d1b3de903e1f.json')
+const bot=new chatbot('newagent-1-impflf','./newagent-1-impflf-cf563cf4b796.json')
 var app = express();
 const PORT = 3000;
 
@@ -24,21 +24,21 @@ app.get('/', function(req, res) {
     res.redirect('html/appMain.html');
 });
 app.get('/chatbot', (req,res) => {
-    const msg='배달';
+    console.log(req.query.msg);
+    const msg=req.query.msg;
     const id='test-session-id';
 
     bot.send(msg,id).then(r => {
         console.log(r[0].queryResult.fulfillmentText);
+        res.json({msg:r[0].queryResult.fulfillmentText});
         // res.send({
         //     msg:{
         //         text:r[0].queryResult.fulfillmentText
         //     }
         // })
     }).catch(e =>{
-        res.send({
-            msg:{
-                text:'오류'
-            }
+        res.json({
+                msg:'통신 오류에요!! 다시 입력해봐요'
         })
     })
     //console.log(r[0].queryResult.fulfillmentText);
@@ -48,81 +48,17 @@ http.createServer(app).listen(app.get('port'), function() {
     console.log('Server START...' + app.get('port'));
 });
 
-// // projectId: ID of the GCP project where Dialogflow agent is deployed
+
 // const projectId = 'newagent-1-impflf';
-// // sessionId: String representing a random number or hashed user identifier
 // const sessionId = '123456';
-// // queries: A set of sequential queries to be send to Dialogflow agent for Intent Detection
 //  const queries = [
 //    '주문해줘',
-//    '배달', // Tell the bot when the meeting is taking place
-//   'B'  // Rooms are defined on the Dialogflow agent, default options are A, B, or C
+//    '배달',
 //  ]
-// // languageCode: Indicates the language Dialogflow agent should use to detect intents
+// // languageCode: 
 //  const languageCode = 'ko';
 //
-// async function detectIntent(
-//     projectId,
-//     sessionId,
-//     query,
-//     contexts,
-//     languageCode
-// ) {
-//     console.log(1);
-//     // The path to identify the agent that owns the created intent.
-//     const sessionPath = sessionClient.projectAgentSessionPath(
-//         projectId,
-//         sessionId
-//     );
-//
-//     // The text query request.
-//     const request = {
-//         session: sessionPath,
-//         queryInput: {
-//             text: {
-//                 text: query,
-//                 languageCode: languageCode,
-//             },
-//         },
-//     };
-//
-//     if (contexts && contexts.length > 0) {
-//         request.queryParams = {
-//             contexts: contexts,
-//         };
-//     }
-//     console.log(2);
-//     const responses = await sessionClient.detectIntent(request);
-//     console.log(3);
-//     return responses[0];
-// }
-//
-// async function executeQueries(projectId, sessionId, queries, languageCode) {
-//     // Keeping the context across queries let's us simulate an ongoing conversation with the bot
-//     let context;
-//     let intentResponse;
-//     for (const query of queries) {
-//         try {
-//             console.log(`Sending Query: ${query}`);
-//             intentResponse = await detectIntent(
-//                 projectId,
-//                 sessionId,
-//                 query,
-//                 context,
-//                 languageCode
-//             );
-//             console.log('Detected intent');
-//             console.log(
-//                 `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
-//             );
-//             // Use the context from this response for next queries
-//             context = intentResponse.queryResult.outputContexts;
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-// }
-// executeQueries(projectId, sessionId, queries, languageCode);
+
 /*
 ================== 서버 가동 꿀 팁 ===================
 >nodemon index.js
--- public/js/appMain.js
+++ public/js/appMain.js
@@ -39,11 +39,83 @@
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
         $('.send_message').click(function (e) {
-            return sendMessage(getMessageText());
+
+            let msg=$(".message_input").val();
+            console.log(msg);
+            sendMessage(getMessageText());
+            $.ajax({
+                type : "GET",
+                url : "../../chatbot",
+                dataType : "json",
+                data : {msg:msg},
+                error : function(){
+                    alert("통신실패!!!!");
+                },
+                success : function(Parse_data){
+                    console.log("success data : "+Parse_data);
+                    //$("#Parse_Area").html(Parse_data); //div에 받아온 값을 넣는다.
+                    let dt=JSON.stringify(Parse_data);
+                    let dt1=dt.substring(8,dt.length-2);
+                    //8부터 split
+                    //alert("통신 데이터 값 : " + dt1);
+                    return sendMessage(dt1+"<br><img src='./1월.png'></img>");
+
+                }
+
+            });
         });
         $('.message_input').keyup(function (e) {
             if (e.which === 13) {
-                return sendMessage(getMessageText());
+                let msg=$(".message_input").val();
+                console.log(msg);
+                sendMessage(getMessageText());
+                $.ajax({
+                    type : "GET",
+                    url : "../../chatbot",
+                    dataType : "json",
+                    data : {msg:msg},
+                    error : function(){
+                        alert("통신실패!!!!");
+                    },
+                    success : function(Parse_data){
+                        console.log("success data : "+Parse_data);
+                        //$("#Parse_Area").html(Parse_data); //div에 받아온 값을 넣는다.
+                        let dt=JSON.stringify(Parse_data);
+                        let dt1=dt.substring(8,dt.length-2);
+                        //8부터 split
+                        let path_img=''
+                        if(dt1=='1월 학사일정입니다.')
+                            path_img='./img/month1.png'
+                        else if(dt1=='2월 학사일정입니다.')
+                            path_img='./img/month2.png'
+                        else if(dt1=='3월 학사일정입니다.')
+                            path_img='./img/month3.png'
+                        else if(dt1=='4월 학사일정입니다.')
+                            path_img='./img/month4.png'
+                        else if(dt1=='5월 학사일정입니다.')
+                            path_img='./img/month5.png'
+                        else if(dt1=='6월 학사일정입니다.')
+                            path_img='./img/month6.png'
+                        else if(dt1=='7월 학사일정입니다.')
+                            path_img='./img/month7.png'
+                        else if(dt1=='8월 학사일정입니다.')
+                            path_img='./img/month8.png'
+                        else if(dt1=='9월 학사일정입니다.')
+                            path_img='./img/month9.png'
+                        else if(dt1=='10월 학사일정입니다.')
+                            path_img='./img/month10.png'
+                        else if(dt1=='11월 학사일정입니다.')
+                            path_img='./img/month11.png'
+                        else if(dt1=='12월 학사일정입니다.')
+                            path_img='./img/month12.png'
+                        if(path_img=='')
+                            return sendMessage(dt1);
+                        else
+                            return sendMessage(dt1+"<br><img src='"+path_img+"' height='200px' width='500px'></img>");
+
+                    }
+
+                });
             }
         });
         sendMessage('안녕하세오 그저봇이에오');
